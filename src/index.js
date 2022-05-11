676cdeeb5cbab51c1bc2c64199ac79f9a968f379
abiodunraheem <raheemabiodun23@gmail.com>
2022-05-11 23:29:04 +0100

    Add test.js file and update the README.md file

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import './style.css';
+import Task from './todo-list.js';
+
+const { taskForm } = document.forms;
+const { description } = taskForm;
+
+const addTaskInput = document.querySelector('.add-task-input');
+const addBtn = document.querySelector('.add-task-btn');
+const mainTasksCont = document.querySelector('.main-tasks');
+const clearComplete = document.querySelector('.clear-btn');
+
+const task = new Task();
+
+addBtn.addEventListener('click', () => {
+  task.addTask(description.value);
+  description.value = '';
+});
+addTaskInput.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    task.addTask(description.value);
+  }
+});
+
+mainTasksCont.addEventListener('click', (e) => {
+  if (e.target.id === 'del') {
+    const id = e.target.value;
+    task.deleteTask(id);
+  }
+});
+clearComplete.addEventListener('click', () => { task.clearComplete(); });
+document.addEventListener('DOMContentLoaded', () => {
+  task.showTasks();
+});
+document.addEventListener('DOMContentLoaded', () => { task.actions(); });
